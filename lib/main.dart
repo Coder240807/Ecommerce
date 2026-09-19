3b7c8f2acbfbff6c01088f2520cb2d3a065121f2
Coder240807 <verma.vigya2007@gmail.com>
2026-09-19 11:51:11 +0530

    feat: Implement MainScreen with bottom navigation and integrate HomeScreen

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,51 @@ import 'package:flutter/material.dart';
 import 'home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MainScreen());
 }
 
-class MyApp extends StatelessWidget {
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+  void _navigateBottomBar(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  final List _pages = [HomeScreen()];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'E-Commerce App',
+      theme: ThemeData(useMaterial3: true, primarySwatch: Colors.blue),
+      home: Scaffold(
+        body: _pages[_currentIndex],
+        bottomNavigationBar: NavigationBar(
+          selectedIndex: _currentIndex,
+          onDestinationSelected: _navigateBottomBar,
+          destinations: const [
+            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+            NavigationDestination(
+              icon: Icon(Icons.favorite_rounded),
+              label: 'Wishlist',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/*class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
@@ -18,4 +59,4 @@ class MyApp extends StatelessWidget {
       home: const HomeScreen(),
     );
   }
-}
+}*/
